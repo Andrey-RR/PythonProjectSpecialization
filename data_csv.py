5a002594bad8eab008465271973d2af364ba96ae
andrey <13rred@gmail.com>
2024-03-19 01:18:01 +0300

    check_commit

--- data_csv.py
+++ data_csv.py
@@ -1,17 +1,14 @@
 import csv
 
-def write_info(info):
-    with open("data.csv", "a", encoding="utf-8") as file:
-        file.write(info)
-    return "Заметка добавлена"
-
-
-
-
 def number_lines():
     with open("data.csv", "r",encoding="utf-8") as file:
         lst = file.readlines()
         return lst
+
+def write_info(info):
+    with open("data.csv", "a", encoding="utf-8") as file:
+        file.write(info)
+    return "Заметка добавлена"
 
 
 
